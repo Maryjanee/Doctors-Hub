b9172362240cb3277bd9e37d3e6188c260159154
Akpagbue Maryjane <akpagbuemaryjane@gmail.com>
2021-03-04 23:24:40 +0100

    Create Login Component

---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const Login = () => (
+  <div>
+    <p>Login Page</p>
+    <Link to="/">Home Page</Link>
+    <br />
+    <Link to="/signup">login</Link>
+  </div>
+);
+export default Login;
