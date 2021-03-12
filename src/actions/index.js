2c301473c44e7f77b8eaf6898d4c89d19660a82f
Akpagbue Maryjane <akpagbuemaryjane@gmail.com>
2021-03-12 15:09:33 +0100

    Add Appointment actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,43 +1,64 @@
 import AppointmentService from '../services/appointments_service';
 import DoctorsService from '../services/doctors-service';
+import { getUserIdFromToken } from '../utils/token';
 import {
-  GETDOCTORSPENDING, GETDOCTORSSUCCESS, GETDOCTORSERROR,
-  GETUSERAPPONTMENTPENDING, GETUSERAPPOINTMENTSUCCESS,
-  GETUSERAPPOINTMENTERROR,
+  GET_DOCTORS_PENDING, GET_DOCTORS_SUCCESS, GET_DOCTORS_ERROR,
+  GET_USER_APPOINTMENTS_PENDING, GET_USER_APPOINTMENTS_SUCCESS,
+  GET_USER_APPOINTMENTS_ERROR, CREATE_USER_APPOINTMENT_SUCCESS,
 } from '../types';
 
 export const getDoctorPending = () => ({
-  type: GETDOCTORSPENDING,
+  type: GET_DOCTORS_PENDING,
 });
 
 export const getDoctorsSuccess = alldoctors => ({
-  type: GETDOCTORSSUCCESS,
+  type: GET_DOCTORS_SUCCESS,
   payload: alldoctors,
 });
 
 export const getDoctorsError = error => ({
-  type: GETDOCTORSERROR,
+  type: GET_DOCTORS_ERROR,
   payload: error,
 });
 
 export const getUserAppointmentsPending = () => ({
-  type: GETUSERAPPONTMENTPENDING,
+  type: GET_USER_APPOINTMENTS_PENDING,
 });
 
-export const getUserAppointmentSuccess = appointments => ({
-  type: GETUSERAPPOINTMENTSUCCESS,
+export const getUserAppointmentsSuccess = appointments => ({
+  type: GET_USER_APPOINTMENTS_SUCCESS,
   payload: appointments,
 });
 
-export const getUserAppointmentError = error => ({
-  type: GETUSERAPPOINTMENTERROR,
+export const getUserAppointmentsError = error => ({
+  type: GET_USER_APPOINTMENTS_ERROR,
   payload: error,
 });
 
-export const fetchDoctors = () => async dispatch => {
-  dispatch(getDoctorPending);
+export const createUserAppointmentSuccess = userDetails => ({
+  type: CREATE_USER_APPOINTMENT_SUCCESS,
+  payload: userDetails,
+});
+
+export const createUserAppointmentError = error => ({
+  type: CREATE_USER_APPOINTMENT_SUCCESS,
+  payload: error,
+});
+
+export const createAppointment = details => async dispatch => {
   try {
-    const { data: doctors } = await DoctorsService.getDoctors();
+    const appointmentDetails = await AppointmentService.createAppointment(details);
+    dispatch(createUserAppointmentSuccess(appointmentDetails));
+  } catch (error) {
+    const errorMsg = error.error;
+    dispatch(createUserAppointmentError(errorMsg));
+  }
+};
+
+export const fetchDoctors = () => async dispatch => {
+  dispatch(getDoctorPending());
+  try {
+    const doctors = await DoctorsService.getDoctors();
     dispatch(getDoctorsSuccess(doctors));
   } catch (error) {
     const errorMsg = error.error;
@@ -46,12 +67,13 @@ export const fetchDoctors = () => async dispatch => {
 };
 
 export const fetchAppointments = () => async dispatch => {
-  dispatch(getUserAppointmentsPending);
+  dispatch(getUserAppointmentsPending());
+  const userId = getUserIdFromToken();
   try {
-    const { data: appointments } = await AppointmentService.getAppointment();
-    dispatch(getUserAppointmentSuccess(appointments));
+    const appointments = await AppointmentService.getAppointment(userId);
+    dispatch(getUserAppointmentsSuccess(appointments));
   } catch (error) {
     const errorMsg = error.error;
-    dispatch(getUserAppointmentError(errorMsg));
+    dispatch(getUserAppointmentsError(errorMsg));
   }
 };
