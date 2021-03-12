ee46af9bb057d84e8a97514b895f4f5c61c71a8f
Akpagbue Maryjane <akpagbuemaryjane@gmail.com>
2021-03-12 15:11:02 +0100

    Update App to fetch doctors on component mount

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,35 @@
-import { BrowserRouter, Route } from 'react-router-dom';
-import Home from './Home';
+import { useEffect } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Login from './Login';
 import SignUp from './Signup';
+import Doctors from '../containers/Doctors';
+import Doctor from '../containers/Doctor';
+import { fetchDoctors } from '../actions/index';
+import ProtectedRoute from './ProtectedRoute';
+import Appointment from './Appointments';
+import FourOhFour from './404';
+import '../styles/App.scss';
 
-const App = () => (
-  <BrowserRouter>
-    <>
-      <Route exact path="/" component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={SignUp} />
-    </>
-  </BrowserRouter>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (localStorage.getItem('auth-token')) {
+      dispatch(fetchDoctors());
+    }
+  }, []);
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/signup" component={SignUp} />
+        <ProtectedRoute path="/doctors" component={Doctors} />
+        <ProtectedRoute path="/doctor/:doctorId" component={Doctor} />
+        <ProtectedRoute path="/appointments/:userId" component={Appointment} />
+        <Route path="/*" component={FourOhFour} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
